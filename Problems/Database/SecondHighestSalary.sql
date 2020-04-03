0437cebfc369f78f5efef9e1c63dec50ee68f54b
Miguel Angel Gutiérrez <magutierrezsistemas@gmail.com>
2020-04-03 07:38:21 +0200

    Update SecondHighestSalary.sql

--- Problems/Database/SecondHighestSalary.sql
+++ Problems/Database/SecondHighestSalary.sql
@@ -1,1 +1,1 @@
-select Salary as SecondHighestSalary from Employee ORDER BY Salary DESC LIMIT 1,1;
+select ifnull( (select distinct Salary from Employee ORDER BY Salary DESC LIMIT 1,1), null ) as SecondHighestSalary;

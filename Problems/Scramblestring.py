fdca86c0c0ffb3ff192357cda6759415242096c5
Miguel Angel Gutiérrez <magutierrezsistemas@gmail.com>
2020-11-18 06:15:33 +0100

    Create Scramblestring.py

---
+++ Problems/Scramblestring.py
@@ -0,0 +1,7 @@
+class Solution:
+    def isScramble(self, s1: str, s2: str) -> bool:
+        if s1 == s2: return True
+        if sorted(s1)!=sorted(s2): return False
+        return any(self.isScramble(s1[:i],s2[:i]) and self.isScramble(s1[i:],s2[i:]) or
+                   self.isScramble(s1[:i],s2[-i:]) and self.isScramble(s1[i:],s2[:-i]) 
+                   for i in range(1,len(s1)))        

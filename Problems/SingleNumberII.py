ea16c6e3a7ac97ce5554f86de05be576493a8fac
Miguel Angel Gutiérrez <magutierrezsistemas@gmail.com>
2021-02-01 06:43:46 +0100

    Create SingleNumberII.py

---
+++ Problems/SingleNumberII.py
@@ -0,0 +1,6 @@
+from collections import Counter
+
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        reps = collections.Counter(nums)
+        return min(reps, key=reps.get)

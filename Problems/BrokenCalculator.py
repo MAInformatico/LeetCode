217aa614840aeb67a1e60c1dac68d01a0c6f5e91
Miguel Angel Gutiérrez <magutierrezsistemas@gmail.com>
2022-03-23 19:43:43 +0100

    Update BrokenCalculator.py

--- Problems/BrokenCalculator.py
+++ Problems/BrokenCalculator.py
@@ -1,7 +1,5 @@
 class Solution:
     def brokenCalc(self, startValue: int, target: int) -> int:
-        if startValue >= target:
-            return startValue - target
         
         def count(target,startValue):
             if startValue >= target:

bb6b7f345729e7d308b544d53d491b738c45228d
Miguel Angel Gutiérrez <magutierrezsistemas@gmail.com>
2022-04-17 09:09:38 +0200

    Create ReshapeMatrix.py

---
+++ ProgrammingSkills/ReshapeMatrix.py
@@ -0,0 +1,15 @@
+class Solution:
+    def matrixReshape(self, mat: List[List[int]], r: int, c: int) -> List[List[int]]:
+        values = []
+        for i in mat:
+            values += i
+        
+        if len(values) != r*c:
+            return mat
+        else:
+            reshapedMatrix = []
+            for row in range(r):
+                matRow = values[:c]
+                values[:] = values[c:]
+                reshapedMatrix.append(matRow)
+            return reshapedMatrix
